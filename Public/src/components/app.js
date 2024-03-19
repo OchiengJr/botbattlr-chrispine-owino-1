1df11941576369f10f7b168ea71dbf87cf999295
OchiengJr <chrispine.ochieng@student.moringaschool.com>
2024-03-19 10:36:18 +0300

    Phase 2 Code Challenge: Bot Battlr

---
+++ Public/src/components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BotsPage from "./BotsPage";
+
+// The root component of the application
+function App() {
+  // Render the BotsPage component inside a containing div
+  return (
+    <div className="App">
+      <BotsPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ Public/src/components/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+
+// Render the App component into the root element of the HTML document
+ReactDOM.render(<App />, document.getElementById("root"));
